5046125aebcc9e1fd9115f268bd8e9d604757b05
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2020-10-01 19:33:57 -0400

    express validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,20 @@
 const express = require('express')
-
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check')
 
 // @ route Post api/users
 // @ desc Register a user
 // @ access Public
-router.post('/', (req, res) => {
+router.post('/', [
+    check('name', 'name is required').not().isEmpty(),
+    check('email', 'please include a valid email').isEmail(),
+    check('password', 'please enter a password with 6 or more chars').isLength({ min: 6 }),
+
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
     res.send('Register a user')
 })
 
