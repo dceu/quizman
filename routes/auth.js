9654f83a48f4ee3474b1a9e4fdd0ac43315f133d
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2020-10-01 09:09:58 -0400

    scaffold some routes

---
+++ routes/auth.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router();
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+
+// @route   GET api/users
+// @desc    Get logged in user
+// @access  Private
+router.get('/', async (req, res) => {
+    // res.send('Get logged in user');
+    try {
+        // const user = await User.findByID(req.user.id).select('-password');
+        // res.json(user)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+});
+
+// @route   POST api/users
+// @desc    Auth user & get JWT
+// @access  Public
+
+// router.post('/', [
+//validator params
+//], async (req, res) => {
+//
+// }
+
+module.exports = router;
---
+++ routes/quizzes.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router();
+// const auth = require('../middleware/auth')
+// const {check, validationResult} = require('express-validator/check');
+
+// const User = require('../models/User')
+// const Quiz = require('../models/Contact')
+
+///////////////////////////////////////////////
+//  @route      GET api/quizzes
+//  @desc       GET all users Quizzes
+//  @access     Private
+
+
+///////////////////////////////////////////////
+//  @route      POST api/quizzes
+//  @desc       Add new quiz
+//  @access     Private
+
+
+///////////////////////////////////////////////
+//  @route      PUT api/quizzes/:id
+//  @desc       Update quiz
+//  @access     Private
+
+///////////////////////////////////////////////
+//  @route      DELETE api/quizzes/:id
+//  @desc       Delete a quiz
+//  @access     Private
+
+module.exports = router;
