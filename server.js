61d269ec5c1cc405360a9cd72771b86e5c52aedf
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2020-10-01 09:09:45 -0400

    define routes

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express')
 
 const app = express();
+
+// Connect DB
+
+// Init Middleware
+
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/quizzes', require('./routes/quizzes'));
+app.use('/api/auth', require('./routes/auth'));
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
