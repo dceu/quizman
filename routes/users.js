0f6fa961b610072eede7c45e93dcbcb287bf2111
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2020-10-01 20:22:19 -0400

    UserSchema added to user route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check')
+const { check, validationResult } = require('express-validator/check');
+const User = require('../models/User');
 
 // @ route Post api/users
 // @ desc Register a user
@@ -10,12 +11,44 @@ router.post('/', [
     check('email', 'please include a valid email').isEmail(),
     check('password', 'please enter a password with 6 or more chars').isLength({ min: 6 }),
 
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
-    res.send('Register a user')
+
+    const { name, email, password } = req.body;
+    try {
+
+        let user = await User.findOne({ email });
+
+        // Check if user already exists
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists' }); //return if true
+        }
+
+        //  Else instantiate new User
+        user = new User({
+            name,
+            email,
+            password
+
+
+        });
+
+        // Encrypt password before passing it to DB
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        // Pass to db
+        await user.save();
+
+    } catch (err) {
+        // make an error handler
+        console.error(err.message);
+        res.status(500).send('Server error')
+
+    }
 })
 
 module.exports = router;
