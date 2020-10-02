44c62e6faac207a549c59c13f0b4e7ab4ff3adb5
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2020-10-01 20:52:26 -0400

    check, validationResult, UserSchema

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const { check, validationResult } = require('express-validator/check');
 
+const User = require('../models/User')
 
 // @route   GET api/users
 // @desc    Get logged in user
