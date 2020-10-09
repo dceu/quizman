aed6458aacfc4e6602cf48e43c90f100353dadef
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2020-10-09 01:45:08 -0400

    lame about Page

---
+++ quizman-client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p className="">
+                A Full Stack React app for making/taking quizzes
+            </p>
+            <p className='bg-dark p'>
+                <strong>Version:</strong> .5
+            </p>
+        </div>
+    )
+}
+
+export default About
