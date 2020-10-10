4bb1e4a26e09178b491537719c7de1571044e1f7
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2020-10-10 02:00:21 -0400

    changed with updated styling

--- quizman-client/src/components/layout/Navbar.js
+++ quizman-client/src/components/layout/Navbar.js
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom'
 
 function Navbar({ title, icon }) {
     return (
-        <div className="App-header">
+        <div className="navbar bg-primary">
             <h1>
                 <i className={icon} /> {title}
             </h1>
             <ul>
-                <li className="App-link">
+                <li>
                     <Link to='/'>Home</Link>
                 </li>
-                <li className="App-link">
+                <li>
                     <Link to='/about'>About</Link>
                 </li>
             </ul>
