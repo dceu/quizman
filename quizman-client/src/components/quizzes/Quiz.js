993d198ca776b3d994b3a8e454a38532dd7e33c0
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2020-10-10 02:01:42 -0400

    scaffolding for single quiz

---
+++ quizman-client/src/components/quizzes/Quiz.js
@@ -0,0 +1,27 @@
+// import React from 'react'
+// import PossibleAnswers from './PossibleAnswers'
+
+// const Quiz = ({ quiz }) => {
+
+
+
+//     const { id, questions } = quiz;
+//     return (
+//         <ul>
+//             {questions.map((question, index) =>
+//                 (<li><h4 className="text-primary text-left">
+//                     {question.question} <i className="fas fa-question" />
+//                 </h4>
+//                     <PossibleAnswers
+//                         key={id}
+//                         distractors={question.distractors}
+//                         answer={question.answer} />
+//                 </li>)
+
+
+//             )}
+//         </ul>
+//     )
+// }
+
+// export default Quiz
